02cc4ad71126db3239162f8f30149bd342c2893a
Matthew Scharley <matt.scharley@gmail.com>
2021-03-09 10:47:57 +1100

    feat: initial pass on a reproduction

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import { GraphQLSchema, GraphQLString, GraphQLObjectType, graphql } from "graphql";
+
+const port = parseInt(process.env.PORT ?? "8080", 10);
+const apiKey = process.env.TEST_API_KEY ?? "Hello world";
+console.log("API Key:", apiKey);
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      helloWorld: {
+        type: GraphQLString,
+        resolve() {
+          return 'Hello!';
+        },
+      },
+    },
+  }),
+});
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  if (req.headers['api-key'] == null) {
+    done(new Error("Invalid request"));
+  }
+  next();
+});
+app.use("/graphql", (req, res, next) => {
+  console.log("Got a new request:", req.body);
+  graphql(schema, req.body.query).then((result) => {
+    console.log("Result:", result);
+    res.send(result);
+  });
+});
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(400);
+    res.send("Invalid request");
+    next();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/graphql`);
+});
